6939e9b216cbc69e1a7b90d6375b17544d1c303d
summer <summer@socialclub.co.kr>
2022-08-24 17:23:41 +0900

    Add. 🌸서버리스 환경 설정 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import {Controller, Get} from "@nestjs/common";
-import {AppService} from "./app.service";
-import {ApiOperation} from "@nestjs/swagger";
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("test")
+  @Get('')
   @ApiOperation({
-    summary: "",
+    summary: '',
+  })
+  getStart(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('test')
+  @ApiOperation({
+    summary: '',
   })
   getHello(): string {
     return this.appService.getHello();
